e76793a61d3b78530e5b5f27212d6cb24c626111
devbryan0225 <devbryan0225@users.noreply.github.com>
2021-05-12 23:26:31 +0800

    Install npm and tailwindcss library.

---
+++ CovidTestCentres/CovidTestCentres.Client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: {
+        enabled: true,
+        content: ['./**/*.html','./**/*.razor']
+    },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
