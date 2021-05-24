19efe24067151cc50d503fe368f266904f3c0b23
devbryan0225 <devbryan0225@users.noreply.github.com>
2021-05-24 22:55:11 +0800

    Draft layout.

---
+++ CovidTestCentres/CovidTestCentres.Client/Models/ClinicModel.cs
@@ -0,0 +1,10 @@
+namespace CovidTestCentres.Client.Models
+{
+    public class Clinic
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+        public string Link { get; set; }
+    }
+}
